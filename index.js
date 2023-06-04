eb132027c0011cbe0069a927cb47db4d60a193e2
milpen <milpen@mail.ru>
2023-06-04 20:54:59 +0500

    random added

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
+"use strict"
 const input1=document.getElementById("name");
 const buttonSubmit=document.getElementById("btn");
 const input2=document.getElementById('link');
 const input3=document.getElementById('comment');
+const no=document.getElementById("no");
 
 function userName (str1) {
 let result1=str1.trim();
 
- if(result1 ==='') {
-    return"Имя не введено";
-}
+ if(result1 ==='' || no.checked ||result1 ===' ') {
+    return"Username";
+} else {
 result1 = result1[0].toUpperCase()+result1.slice(1).toLowerCase();
-  //console.log(result1);
 return result1;
+}
 }
 
 function imageLink () {
   const parentImageLink =document.querySelector("#chat");
+  const arrayAvatars = [
+    "./images/avatar1.png",
+    "./images/avatar2.png",
+    "./images/avatar3.png",
+    "./images/avatar4.png",
+    "./images/avatar5.png",
+  ];
   const image=document.createElement('img');
-  image.style.width='150px';
+ if (input2.value ==="" ||input2.value ===" ") {
+  const randomAvatar = arrayAvatars[Math.floor(Math.random() * arrayAvatars.length)];
+  image.src=randomAvatar;
+ } else {
   image.src=input2.value;
-  image.alt="UserAvatar";
-  parentImageLink.appendChild(image);
+ }
+ image.style.width='150px';
+ parentImageLink.appendChild(image);
 }
 
 function checkSpam(str2) {
